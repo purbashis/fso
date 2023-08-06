b06cec7ebf6835a7d633e275731634601884066c
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-08-06 11:55:56 +0530

    Exercises 1.1.-1.2.

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,35 +1,54 @@
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>Hello  {props.name},you are {props.age}years old </p>
-    </div>
-  )
-}
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
-    </div>
-  )
-}
-const App = () => {
+import React from 'react';
 
-  const name = 'peter'
-  const age =25
-  const friends = [
-    { name: 'Pit', age: 4 },
-    { name: 'Mavi', age: 10 },
-  ]
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Content = (props) => {
   return (
     <>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
-      <Footer />
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
     </>
-  )
-}
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>Total number of exercises: {props.total}</p>
+  );
+};
+
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+/* eslint-disable */
+
 export default App;
